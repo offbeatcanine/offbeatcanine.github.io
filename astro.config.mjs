6cc5039e9a24c72aef11c444658957fb5684b83d
offbeatcanine <166252451+offbeatcanine@users.noreply.github.com>
2025-02-23 20:34:52 -0500

    new reviews section on homepage (#3)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    alpinejs(),
   ],
 });
+
